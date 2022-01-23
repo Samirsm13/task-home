844a39f0db76035024e25fcdcf0c2b3742028447
Samirsm13 <samirsm@code.edu.az>
2022-01-23 16:46:35 +0400

    Add project files.

---
+++ ConsoleApp.Home/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp.Home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+                
+                
+                  
+                
+                
+            }
+
+           
+        
+            
+                
+            
+
+           
+        }
+    }
+}
